c80913cf16a5b365abe4e1d207e25803588d7727
Sodvadiyavidhi <200429730@student.georgianc.on.ca>
2020-01-16 21:37:49 -0500

    first commit

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            
+            string[] product ={ "Laptop","Tablet","Desktop" };
+            ViewBag.product = product;
+            return View();
+        }
+        public string Details(string product)
+        {
+            string message = HttpUtility.HtmlEncode("You selected product:" +product);
+            return message;
+            
+        }
+    }
+}
